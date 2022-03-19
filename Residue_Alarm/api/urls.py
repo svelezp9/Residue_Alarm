3412659e8a33f2345f125f42c0867b6dec9cba6f
Sebastian Velez <svelezp9@eafit.edu.co>
2022-03-19 12:04:13 -0500

    login api, y register api

--- Residue_Alarm/api/urls.py
+++ Residue_Alarm/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('profiles',views.UserProfileViewSet)
 
 urlpatterns = [
-    path('',views.descriptionAPI().as_view()),
+    path('/description',views.descriptionAPI().as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
+    path('',include(router.urls))
 ]
--- Residue_Alarm/api/views.py
+++ Residue_Alarm/api/views.py
@@ -1,7 +1,13 @@
-from email import message
-from urllib import response
+from tokenize import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from . import serializer,models , permissions
 
 class descriptionAPI(APIView):
 
@@ -11,3 +17,15 @@ class descriptionAPI(APIView):
         ]
 
         return Response({'message' : 'Hello', 'descriptionAPI':description})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    '''Creación y update del user'''
+    serializer_class = serializer.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+class UserLoginApiView(ObtainAuthToken):
+    '''Token de autenticación para el usuairo'''
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
